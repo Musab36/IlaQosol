9e5cf7d719af0d319d0c8b174769a632dfab750e
Musab36 <musabhd@gmail.com>
2018-06-19 00:54:26 +0300

    setting up categories model

---
+++ app/src/main/java/com/salajim/musab/ilaqosol/adapters/JokesCategoriesAdapter.java
@@ -0,0 +1,4 @@
+package com.salajim.musab.ilaqosol.adapters;
+
+public class JokesCategoriesAdapter {
+}
---
+++ app/src/main/java/com/salajim/musab/ilaqosol/models/Categories.java
@@ -0,0 +1,13 @@
+package com.salajim.musab.ilaqosol.models;
+
+public class Categories {
+    private String joke;
+
+    public Categories(String joke) {
+        this.joke = joke;
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+}
