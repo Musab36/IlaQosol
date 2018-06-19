7f188a18139f5ed196bea55e24e5687b14d8080e
Musab36 <musabhd@gmail.com>
2018-06-19 04:26:22 +0300

    Displaying dummy data with JazzyListView

--- app/src/main/java/com/salajim/musab/ilaqosol/activities/MainActivity.java
+++ app/src/main/java/com/salajim/musab/ilaqosol/activities/MainActivity.java
@@ -1,15 +1,100 @@
 package com.salajim.musab.ilaqosol.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.salajim.musab.ilaqosol.R;
+import com.salajim.musab.ilaqosol.adapters.JokesCategoriesAdapter;
+import com.salajim.musab.ilaqosol.models.Categories;
+import com.twotoasters.jazzylistview.JazzyHelper;
+import com.twotoasters.jazzylistview.JazzyListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private JokesCategoriesAdapter mAdapter;
+    private List<Categories> categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        categories = new ArrayList<>();
+
+        JazzyListView jazzyListView = new JazzyListView(this);
+        mAdapter = new JokesCategoriesAdapter(categories, getApplicationContext());
+        jazzyListView.setAdapter(mAdapter);
+
+        jazzyListView.setTransitionEffect(JazzyHelper.CARDS);
+        setContentView(jazzyListView);
+
+        displayJokes();
+    }
+
+    private void displayJokes() {
+        Categories category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+        category = new Categories("Sheko qosol badan oo Juxa eh");
+        categories.add(category);
+
+
+        mAdapter.notifyDataSetChanged();
     }
 }
