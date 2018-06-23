187c115205784f26451906c34af94ba5d6007e74
Musab36 <musabhd@gmail.com>
2018-06-24 02:41:59 +0300

    Saving to SharedPreferences

--- app/src/main/java/com/salajim/musab/ilaqosol/activities/DetailActivity.java
+++ app/src/main/java/com/salajim/musab/ilaqosol/activities/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.salajim.musab.ilaqosol.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.salajim.musab.ilaqosol.R;
 
@@ -14,6 +17,10 @@ import butterknife.ButterKnife;
 public class DetailActivity extends AppCompatActivity {
    @Bind(R.id.description)
     TextView desc;
+   @Bind(R.id.heart) TextView mHeart;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mDescription;
 
    ActionBar toolbar;
 
@@ -27,8 +34,21 @@ public class DetailActivity extends AppCompatActivity {
         toolbar = getSupportActionBar();
         toolbar.setTitle("Sheeka Qosol Badan");
 
+        mSharedPreferences = getSharedPreferences(mDescription, MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+
         Intent intent = getIntent();
-        String description = intent.getStringExtra("description");
+        final String description = intent.getStringExtra("description");
         desc.setText(description);
+
+        mHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDescription = mSharedPreferences.getString(description, null);
+                mEditor.putString(description, description);
+                mEditor.apply();
+                Toast.makeText(getApplicationContext(), "Kedka ayaa lugu daray!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
