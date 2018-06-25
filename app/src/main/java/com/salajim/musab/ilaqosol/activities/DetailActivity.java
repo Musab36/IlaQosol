4f94204ae15233d18038f4547cb3f416eb3e390e
Musab36 <musabhd@gmail.com>
2018-06-26 02:06:32 +0300

    Saving and retrieving data with SQLite database

--- app/src/main/java/com/salajim/musab/ilaqosol/activities/DetailActivity.java
+++ app/src/main/java/com/salajim/musab/ilaqosol/activities/DetailActivity.java
@@ -1,7 +1,6 @@
 package com.salajim.musab.ilaqosol.activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.salajim.musab.ilaqosol.DatabaseHelper;
 import com.salajim.musab.ilaqosol.R;
 
 import butterknife.Bind;
@@ -18,14 +18,18 @@ public class DetailActivity extends AppCompatActivity {
    @Bind(R.id.description)
     TextView desc;
    @Bind(R.id.heart) TextView mHeart;
-    private SharedPreferences mSharedPreferences;
+
+   DatabaseHelper myDB;
+
+   /*private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
     private String mDescription;
+    */
 
    ActionBar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
@@ -34,21 +38,30 @@ public class DetailActivity extends AppCompatActivity {
         toolbar = getSupportActionBar();
         toolbar.setTitle("Sheeka Qosol Badan");
 
-        mSharedPreferences = getSharedPreferences(mDescription, MODE_PRIVATE);
-        mEditor = mSharedPreferences.edit();
-
         Intent intent = getIntent();
         final String description = intent.getStringExtra("description");
         desc.setText(description);
 
+        myDB = new DatabaseHelper(this);
+
         mHeart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mDescription = mSharedPreferences.getString(description, null);
-                mEditor.putString(description, description);
-                mEditor.apply();
-                Toast.makeText(getApplicationContext(), "Kedka ayaa lugu daray!", Toast.LENGTH_SHORT).show();
+                String newEntry = description;
+                if(newEntry.length() != 0) {
+                    AddData(newEntry);
+                }
             }
         });
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = myDB.addData(newEntry);
+
+        if (insertData == true) {
+            Toast.makeText(getApplicationContext(), "Kedka ayaa lugu daray!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/salajim/musab/ilaqosol/fragments/FavoriteFragment.java
+++ app/src/main/java/com/salajim/musab/ilaqosol/fragments/FavoriteFragment.java
@@ -1,19 +1,34 @@
 package com.salajim.musab.ilaqosol.fragments;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.Toast;
 
+import com.salajim.musab.ilaqosol.DatabaseHelper;
 import com.salajim.musab.ilaqosol.R;
+import com.twotoasters.jazzylistview.JazzyHelper;
+import com.twotoasters.jazzylistview.JazzyListView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavoriteFragment extends Fragment {
+    @Bind(R.id.jazz)
+    JazzyListView jazzyListView;
 
+    DatabaseHelper myDB;
 
     public FavoriteFragment() {
         // Required empty public constructor
@@ -24,7 +39,27 @@ public class FavoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        ButterKnife.bind(this, view);
+
+        myDB = new DatabaseHelper(getActivity());
+
+        ArrayList<String> myList = new ArrayList<>();
+        Cursor data = myDB.getListContents();
+
+        if (data.getCount() == 0) {
+            Toast.makeText(getActivity(), "There are no contents in this list!",Toast.LENGTH_LONG).show();
+        } else {
+            while(data.moveToNext()) {
+                myList.add(data.getString(1));
+                ListAdapter listAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, myList);
+                jazzyListView.setAdapter(listAdapter);
+                jazzyListView.setTransitionEffect(JazzyHelper.CARDS);
+            }
+        }
+
+        return view;
     }
+
 
 }
